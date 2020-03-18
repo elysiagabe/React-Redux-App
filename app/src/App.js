82a82b9de22c75b87da7c932aca98f6184a69032
Elysia Gabe <elysiagabe@gmail.com>
2020-03-18 12:40:39 -0700

    Initial commit

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// Styles
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hawaii Ocean Safety</h1>
+    </div>
+  );
+}
+
+export default App;
